71744e69129b3eeb689d37b50a6c9eeba0fa02aa
apurva74 <vt1733620@gmail.com>
2026-04-12 20:35:01 +0530

    removed unused jwtDecode

--- frontend/src/Components/Register/Register.jsx
+++ frontend/src/Components/Register/Register.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
 import API from "../../api";
 import "./Register.css";
 
